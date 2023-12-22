ef0106f41fe0d46a12d95cfd4b8f9541e33a3085
ddamir2005 <39729763+ddamir2005@users.noreply.github.com>
2023-12-23 02:34:39 +0300

    Update periodictableofelements.py

--- periodictableofelements.py
+++ periodictableofelements.py
@@ -1,8 +1,10 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QMainWindow, QTextEdit, QLineEdit
+import sys
 import csv
 
 def read_csv(periodictable):
     data = []
-    with open('periodictable.csv', 'r',encoding = 'utf-8') as file:
+    with open('periodictable.csv', 'r', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
             data.append(row)
@@ -10,7 +12,7 @@ def read_csv(periodictable):
 
 def get_element_info(data, element_number):
     element_data = {}
-    for i, row in enumerate(data[int(element_number)-1]):
+    for i, row in enumerate(data[int(element_number) - 1]):
         key = ''
         if i == 0:
             key = 'Atomic Number'
@@ -41,34 +43,60 @@ def get_element_info(data, element_number):
         element_data[key] = row
     return element_data
 
-filename = 'periodictable.csv' 
-data = read_csv(filename)
-print('                            Pereodic Table of Elements')
-print(' ',' 1 ',' 2 ',' 3 ',' 4 ',' 5 ',' 6 ',' 7 ',' 8 ',' 9 ',' 10 ',' 11 ',' 12 ',' 13 ',' 14 ',' 15 ',' 16 ',' 17 ',' 18 ')
-print('1',' H')
-print('2',' Li',' Be','                                           ','B','   C','   N','   O','   F','   Ne')
-print('3',' Na',' Mg','                                           ', 'Ai', '  Si ', ' P','   S',   '   C1',  '  Ar')
-print('4',' K','  Ca',' Sc',' Ti',' V','  Cr',' Mn',' Fe',' Co',' Ni','  Cu','  Zn','  Ga','  Ge','  As','  Se','  Br','  Kr')
-print('5',' Rb',' Sr',' Y','  Zr',' Nb',' Mo',' Tc',' Ru',' Rh',' Pd','  Ag','  Cd','  In','  Sn','  Sb','  Te','  I','   Xe')
-print('6',' Cs', ' Ba',' La',' Hf',' Ta',' W','  Re',' Os',' Ir',' Pt','  Au','  Hg','  T1','  Pb','  Bi','  Po','  At','  Rn')
-print('7',' Fr', ' Ra',' Ac',' Rf',' Db',' Sg',' Bh',' Hs',' Mt',' Ds','  Rg','  Cn','  Nh','  F1','  Mc','  Lv','  Ts','  Og')
-while True:
-    element_number = str(input('Enter a symbol or atomic number to examine or Quit to quit: '))
-    if element_number == 'Quit':
-        break
-    if not element_number.isdigit():
-        print("Not correct")
-    if element_number.isdigit() and (1<int(element_number)<118):
-        element_info = get_element_info(data, element_number)
+class PeriodicTableApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Periodic Table of Elements")
+        self.setGeometry(100, 100, 800, 600)
 
+        layout = QGridLayout()
+
+        self.data = read_csv('periodictable.csv')
+        elements = []
+        row = 0
+        col = 0
+        for i in range(1, len(self.data) + 1):
+            element_symbol = self.data[i - 1][1]
+            elements.append(element_symbol)
+            button = QPushButton(element_symbol)
+            button.clicked.connect(lambda checked, element_number=i: self.on_element_click(element_number))
+            layout.addWidget(button, row, col)
+            col += 1
+            if col == 18:
+                col = 0
+                row += 1
+
+        self.text_edit = QTextEdit()
+        layout.addWidget(self.text_edit, row + 1, 0, 1, 18)
+
+        self.input_field = QLineEdit()
+        layout.addWidget(self.input_field, row + 2, 0, 1, 18)
+
+        self.search_button = QPushButton("Поиск")
+        self.search_button.clicked.connect(self.search_element)
+        layout.addWidget(self.search_button, row + 3, 0, 1, 18)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def on_element_click(self, element_number):
+        element_info = get_element_info(self.data, element_number)
+        info_text = ""
         for key, value in element_info.items():
-            print(f'{key}: {value}')
+            info_text += f'{key}: {value}\n'
+        self.text_edit.setPlainText(info_text)
 
-    
-        
-    
-   
-        
-    
-    
+    def search_element(self):
+        element_number = self.input_field.text().strip()
+        if element_number.isdigit() and 1 <= int(element_number) <= len(self.data):
+            self.on_element_click(int(element_number))
+        else:
+            self.text_edit.setPlainText("Некорректно")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = PeriodicTableApp()
+    window.show()
+    sys.exit(app.exec())
